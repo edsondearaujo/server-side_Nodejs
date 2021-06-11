4de1e052af98b0b113cbe43193968661d45cce7d
edsondearaujo <edsondearaujofga@gmail.com>
2021-06-11 00:41:35 -0300

    feat(#3): Add require/node_module

--- index.js
+++ index.js
@@ -1,21 +1,18 @@
-var rectangle = {
-    perimeter: (x,y) => (2*(x+y)),
-    area: (x,y) => (x*y)
-};
+var rectangle = require('./rectangle')
 
 function solveRect(l,b) {
-    console.log("Solving for rectangle with l = " + l + ", and b = " + b);
+    console.log("Solving for rectangle with l = " + l +"m" + ", and b = " + b + "m");
     if( l <= 0 || b <= 0){
         console.log("Rectangle dimensions should be greater than zero: l = " + l + ", and b = " + b)
     }
     else{
-        console.log("The area of the rectangle is " + rectangle.area(l,b));
-        console.log("The perimeter of the rectangle is " + rectangle.perimeter(l,b));
+        console.log("The area of the rectangle is " + rectangle.area(l,b)+"m²");
+        console.log("The perimeter of the rectangle is " + rectangle.perimeter(l,b)+"m"+"\n");
     }
 }
 
 // input example
-solveRect(2,4);
-solveRect(3,5);
-solveRect(0,5);
-solveRect(-3,5);
+solveRect(50,50);
+solveRect(25,50);
+solveRect(30,30);
+solveRect(20,20);
